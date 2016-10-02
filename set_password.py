1a17021de63ab90da8bd23436aed7448228db8c8
bitelxux <bitelxux@gmail.com>
2016-10-02 12:05:00 +0100

    cosmetics

--- set_password.py
+++ set_password.py
@@ -1,10 +1,14 @@
 import os
 import sys
 
-from trac.env import Environment
-from acct_mgr.api import AccountManager
 
-env = Environment(sys.argv[1])
-user, passwd = sys.argv[2], sys.argv[3]
-mgr = AccountManager(env)
-mgr.set_password(user, passwd)
+try:
+  from trac.env import Environment
+  from acct_mgr.api import AccountManager
+  env = Environment(sys.argv[1])
+  user, passwd = sys.argv[2], sys.argv[3]
+  account_manager = AccountManager(env)
+  account_manager.set_password(user, passwd)
+except Exception, e:
+  print "Oops !! %s" % e
+  print "Usage: python set_password <env> <user> <password>"
